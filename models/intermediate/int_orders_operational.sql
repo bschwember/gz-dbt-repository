8fbc2f034ee9e6af27160d3edf19641d524b005a
Bruno Schwember <brunoschwember@gmail.com>
2026-02-11 14:14:55 +0000

    added columsn to int_orders_operational

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,8 +1,7 @@
 SELECT
-    m.orders_id
-    ,m.date_date
+    m.*
+    ,s.* EXCEPT (orders_id)
     ,ROUND(m.margin+s.shipping_fee-s.logcost-s.ship_cost,2) AS operational_margin
-    ,m.quantity
 FROM {{ ref('int_orders_margin')}} m
 LEFT JOIN {{ ref('stg_raw__ship')}} s
 USING (orders_id)
