68305bb55d8afcfb5bbfca3e3ab17516152678ce
Bruno Schwember <brunoschwember@gmail.com>
2026-02-12 16:01:50 +0000

    added working macro and updated int_sales_margin

--- macros/functions.sql
+++ macros/functions.sql
@@ -1,3 +1,9 @@
- {% macro margin_percent(revenue, purchase_cost) %}
-     ROUND(SAFE_DIVIDE({{revenue}}, {{purchase_cost}}),2)
- {% endmacro %}
+{% macro margin_percent(revenue, purchase_cost) %}
+    ROUND(
+SAFE_DIVIDE(
+            {{ revenue }} - {{ purchase_cost }},
+            {{ revenue }}
+        ) * 100,
+        2
+    )
+{% endmacro %}
